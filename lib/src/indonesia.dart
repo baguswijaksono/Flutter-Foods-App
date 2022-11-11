7ee0b527d22e22418cf4c543fa219b6dedd1fdae
baguswijaksono <baguswijaksono292@gmail.com>
2022-11-12 03:24:40 +0700

    fifth commit

--- lib/src/indonesia.dart
+++ lib/src/indonesia.dart
@@ -6,16 +6,54 @@ class indonesian extends StatelessWidget{
      double dev_width = MediaQuery.of(context).size.width;//variabel buat nyari lebar device
      double dev_height = MediaQuery.of(context).size.height; //variabel buat nyari panjang device
     return Scaffold(
+            backgroundColor: Color.fromRGBO(13, 13, 13, 1.0),
             body: SafeArea(
         child: Column(
           crossAxisAlignment:CrossAxisAlignment.stretch, 
           children: [ 
                     SizedBox(
-                       height: dev_height-78,
+                       height: dev_height,
                     width: dev_width,
                     child: ListView(
                       scrollDirection: Axis.vertical,
                       children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Container( child: Padding(
+                              padding: const EdgeInsets.all(8.0),
+                                child: IconButton(
+                                  icon: const Icon(
+                                    Icons.navigate_before,
+                                    color: Colors.white,),
+                                    onPressed: () {
+                                      Navigator.pop(context);
+                                      },
+                                      ),
+                                      ),
+                                      ),
+                              Container( child: Padding(
+                              padding: const EdgeInsets.all(8.0),
+                                child: IconButton(
+                                  icon: const Icon(
+                                    Icons.search_sharp,
+                                    color: Colors.white,),
+                                    onPressed: () {
+                                      Navigator.pop(context);
+                                      },
+                                      ),
+                                      ),
+                                      ),
+                          ],
+                        ),
+                        Container(
+                          child: Column(
+                            children: [
+                              Text('REGIONAL\nFOODS',style: TextStyle(color: Colors.white,)),
+                              Text('of',style: TextStyle(color: Colors.white,)),
+                              Text('Indonesian',style: TextStyle(color: Colors.white,)),
+                            ],
+                          ))
                           ///aa
                       ]
                       ),
