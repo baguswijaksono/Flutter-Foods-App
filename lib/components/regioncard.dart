1bf97812d89b03f0d4e98f5b2c4a683e3a386122
baguswijaksono <baguswijaksono292@gmail.com>
2022-12-03 14:58:54 +0700

    buat regional card jadi widget

--- lib/components/regioncard.dart
+++ lib/components/regioncard.dart
@@ -1,11 +1,38 @@
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_application_akhir/src/indonesia.dart';
 
 class regioncard extends StatelessWidget {
   const regioncard({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return GestureDetector(
+
+                onTap:() {Navigator.push(
+                                          context,
+                                          MaterialPageRoute(
+                                              builder: (context) => indonesian()));},
+                                                child: ClipRRect(
+                                borderRadius: BorderRadius.circular(10),
+                                        child: Stack(
+            children: <Widget>[
+              Container(
+                alignment: Alignment.center,
+                child: Image.asset(
+                'images/ina.jpg',
+                fit: BoxFit.fill,
+                ),
+              ),
+              Container(
+                width: 200,
+                  alignment: Alignment.center,
+                  child: Text(
+                      'Indonesian',
+                      style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 22.0, ),                     
+                  ),),
+            ],
+          ),
+                                ),
+);
   }
 }
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_akhir/components/botnavbar.dart';
+import 'package:flutter_application_akhir/components/regioncard.dart';
 import 'package:flutter_application_akhir/pages/search.dart';
 import 'package:flutter_application_akhir/screen/food_detail_screen.dart';
 import 'package:flutter_application_akhir/src/indonesia.dart';
@@ -195,165 +196,23 @@ Padding(
         
   Padding(
     padding: const EdgeInsets.all(4.0),
-    child: GestureDetector(
-
-                onTap:() {Navigator.push(
-                                          context,
-                                          MaterialPageRoute(
-                                              builder: (context) => indonesian()));},
-                                                child: ClipRRect(
-                                borderRadius: BorderRadius.circular(10),
-                                        child: Stack(
-            children: <Widget>[
-              Container(
-                alignment: Alignment.center,
-                child: Image.asset(
-                'images/ina.jpg',
-                fit: BoxFit.fill,
-                ),
-              ),
-              Container(
-                width: 200,
-                  alignment: Alignment.center,
-                  child: Text(
-                      'Indonesian',
-                      style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 22.0, ),                     
-                  ),),
-            ],
-          ),
-                                ),
-),
+    child: regioncard()
   ),
   Padding(
     padding: const EdgeInsets.all(4.0),
-    child: GestureDetector(
-
-                onTap:() {Navigator.push(
-                                          context,
-                                          MaterialPageRoute(
-                                              builder: (context) => indonesian()));},
-
-                                                child: ClipRRect(
-                                
-                                
-                                borderRadius: BorderRadius.circular(10),
-                                        child: Stack(
-            children: <Widget>[
-              Container(
-                alignment: Alignment.center,
-                child: Image.asset(
-                'images/jpn.jpg',
-                ),
-              ),
-              Container(
-                width: 210,
-                  alignment: Alignment.center,
-                  child: Text(
-                      'Japanesse',
-                      style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 22.0, ),
-                      
-                  ),),
-            ],
-          ),
-                                ),
-
-),
+    child: regioncard()
   ),
   Padding(
     padding: const EdgeInsets.all(4.0),
-    child: GestureDetector(
-                onTap:() {Navigator.push(
-                                          context,
-                                          MaterialPageRoute(
-                                              builder: (context) => indonesian()));},
-
-                                                child: ClipRRect(                                
-                                borderRadius: BorderRadius.circular(10),
-                                        child: Stack(
-            children: <Widget>[
-              Container(
-                alignment: Alignment.center,
-                child: Image.asset(
-                'images/chn.jpg',
-                ),
-              ),
-              Container(
-                width: 210,
-                  alignment: Alignment.center,
-                  child: Text(
-                      'Chinesse',
-                      style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 22.0, ),
-                      
-                  ),),
-            ],
-          ),
-                                ),
-
-),
+    child: regioncard()
   ),
   Padding(
     padding: const EdgeInsets.all(4.0),
-    child: GestureDetector(
-
-                onTap:() {Navigator.push(
-                                          context,
-                                          MaterialPageRoute(
-                                              builder: (context) => indonesian()));},
-
-                                                child: ClipRRect(
-                                borderRadius: BorderRadius.circular(10),
-                                        child: Stack(
-            children: <Widget>[
-              Container(
-                alignment: Alignment.center,
-                child: Image.asset(
-                'images/italy.jpg',
-                ),
-              ),
-              Container(
-                width: 210,
-                  alignment: Alignment.center,
-                  child: Text(
-                      'Italyan',
-                      style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 22.0, ),
-                      
-                  ),),
-            ],
-          ),
-                                ),
-),
+    child: regioncard()
   ),
     Padding(
     padding: const EdgeInsets.all(4.0),
-    child: GestureDetector(
-
-                onTap:() {Navigator.push(
-                                          context,
-                                          MaterialPageRoute(
-                                              builder: (context) => indonesian()));},
-
-                                                child: ClipRRect(
-                                borderRadius: BorderRadius.circular(10),
-                                        child: Stack(
-            children: <Widget>[
-              Container(
-                alignment: Alignment.center,
-                child: Image.asset(
-                'images/krn.jpg',
-                ),
-              ),
-              Container(
-                width: 210,
-                  alignment: Alignment.center,
-                  child: Text(
-                      'Korean',
-                      style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 22.0, ),
-                      
-                  ),),
-            ],
-          ),
-                                ),
-),
+    child: regioncard()
   ),
  ],
          ),
