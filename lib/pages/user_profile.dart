7638957726338cfa84fa4fa0f989d1e3becfd574
baguswijaksono <baguswijaksono292@gmail.com>
2022-12-03 17:42:37 +0700

    tambah navbar ke scafold user profile

--- lib/pages/user_profile.dart
+++ lib/pages/user_profile.dart
@@ -1,12 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_akhir/components/botnavbar.dart';
 
 class user extends StatelessWidget {
   const user({super.key});
 
   @override
   Widget build(BuildContext context) {
+   // double dev_height = MediaQuery.of(context).size.height; //variabel buat nyari panjang device
     return Scaffold(
       backgroundColor: Colors.white,
+      bottomNavigationBar: Padding(
+        padding: const EdgeInsets.only(left: 10,right: 10,bottom: 10),
+        child: Container(
+          decoration: BoxDecoration(                                                   
+    borderRadius:BorderRadius.circular(15),            
+    boxShadow: [                                                               
+        BoxShadow(color: Color.fromRGBO(37, 37, 37, 1.0),),       
+    ],                                                                         
+  ),     
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal:12, vertical: 12 ),//padding container  bottom navbar
+            child: bottom_navigation_bar()
+          ),
+        ),
+      ),
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment:CrossAxisAlignment.stretch, 
+          children: [ 
+                //kotak scroll
+          ],
+          )
+          
+          ),
     );
   }
 }
