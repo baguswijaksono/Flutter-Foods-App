05cccc4551ed0b34f135736eed590a48b0c88226
baguswijaksono <baguswijaksono292@gmail.com>
2022-12-03 14:28:57 +0700

    reset navigasi

---
+++ lib/search.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class searchpage
+ extends StatelessWidget {
+  const searchpage
+  ({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
