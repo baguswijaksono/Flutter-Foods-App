7c8e33769615bc679950d1ce7aa00cc20b99b284
baguswijaksono <baguswijaksono292@gmail.com>
2022-12-22 10:23:00 +0700

    sekedar update

--- lib/components/bannercard.dart
+++ lib/components/bannercard.dart
@@ -1,16 +1,54 @@
+import 'dart:convert';
+import 'package:card_loading/card_loading.dart';
+import 'package:http/http.dart' as http;
 import 'package:common_ui_toolkit/index.dart';
 import 'package:flutter_application_akhir/screen/food_detail_screen.dart';
-
+import 'dart:math';
+// ignore: must_be_immutable
 class banner extends StatelessWidget {
-  const banner({super.key});
+   banner({super.key});
+   Random random = new Random();
+   
+    var allapi='https://api.jsonbin.io/v3/b/6392030fc5b3a64f1bc6ab28?meta=false';
+      Future<List<dynamic>> _fecthListQuotes() async {
+    final result = await http.get(Uri.parse(allapi));
+    return json.decode(result.body);
+  }
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
+    
+    return 
+    FutureBuilder<List<dynamic>>(
+        future: _fecthListQuotes(),
+        builder: (BuildContext context, AsyncSnapshot snapshot) {
+if (snapshot.hasData) {
+            // ignore: unused_local_variable
+            var d=snapshot.data.length ;
+            int randomNumber = random.nextInt(d);
+
+            return GestureDetector(
                 onTap:() {Navigator.push(
                                         context,
                                         MaterialPageRoute(
-                                            builder: (context) => detail_screen(imgurl: '', desc: '', fdnm: '', kal: '22', lmk: '22' ,prtn: '22', vit: '22', b1: '', b2: '', b3: '', b4: '', imgurl2: '', l1: '', l2: '', l3: '', l4: '', b5: '', b6: '',)));},
+                                            builder: (context) => detail_screen(fdnm: snapshot.data[randomNumber]['fdnm'].toString(),
+                                   desc: snapshot.data[randomNumber]['desc'].toString(), 
+                                  imgurl: snapshot.data[randomNumber]['imgurl'].toString(), 
+                                  kal: snapshot.data[randomNumber]['Kalori'], 
+                                  lmk: snapshot.data[randomNumber]['Lemak'], 
+                                  prtn: snapshot.data[randomNumber]['Protein'], 
+                                  vit: snapshot.data[randomNumber]['Karbohidrat'], 
+                                  b1: snapshot.data[randomNumber]['bahan1'],
+                                  b2: snapshot.data[randomNumber]['bahan2'],
+                                  b3: snapshot.data[randomNumber]['bahan3'], 
+                                  b4: snapshot.data[randomNumber]['bahan4'], 
+                                  imgurl2: snapshot.data[randomNumber]['imgurl2'], 
+                                  l1: snapshot.data[randomNumber]['langkah1'], 
+                                  l2: snapshot.data[randomNumber]['langkah2'], 
+                                  l3: snapshot.data[randomNumber]['langkah3'], 
+                                  l4: snapshot.data[randomNumber]['langkah4'], 
+                                  b5: snapshot.data[randomNumber]['bahan5'], 
+                                  b6: snapshot.data[randomNumber]['bahan6'],)));},
                                               child: ClipRRect(  
                               borderRadius: BorderRadius.circular(10),
                                       child: Center(
@@ -46,6 +84,17 @@ class banner extends StatelessWidget {
           ),
                                       ),
                               ),
-  );
-  }
+  );}else {
+            return CardLoading(
+  height: 300,
+  borderRadius: BorderRadius.all(Radius.circular(10)),
+  margin: EdgeInsets.only(bottom: 10),
+);
+          }
+  
+        }
+    );
+
+    
+}
 }
